0cc98a9fb9148e2ac32da03b5bc5c57dfe6d4f86
Allar Klein <allarklein@gmail.com>
2025-11-07 02:04:34 +0200

    feat: replace Lambda/API Gateway with ALB-backed Fargate service

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -4,17 +4,9 @@ import { CdkStack } from "../lib/cdk-stack";
 
 const app = new cdk.App();
 new CdkStack(app, "CdkStack", {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
+  // set up the env to use default aws account
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: process.env.CDK_DEFAULT_REGION,
   },
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-
-  
 });
--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,20 +1,45 @@
-import * as apigateway from "aws-cdk-lib/aws-apigateway";
-import * as lambda from "aws-cdk-lib/aws-lambda-nodejs";
+import * as ecs from "aws-cdk-lib/aws-ecs";
+import * as ecs_patterns from "aws-cdk-lib/aws-ecs-patterns";
 import * as cdk from "aws-cdk-lib/core";
 import { Construct } from "constructs";
-import * as path from "path";
+import path from "path";
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const helloFunction = new lambda.NodejsFunction(this, "HelloFunction", {
-      entry: path.join(__dirname, "../../lambda/hello/index.ts"),
-    });
+    // Constructor for load-balanced fargate service
+    const fargate = new ecs_patterns.ApplicationLoadBalancedFargateService(
+      this,
+      "op-fargate",
+      {
+        cpu: 256, //0.25 of a vCPU
+        memoryLimitMiB: 512,
+        taskImageOptions: {
+          // directory of the Dockerfile
+          image: ecs.ContainerImage.fromAsset(
+            path.join(__dirname, "../../app")
+          ),
+          // port for the app in the container
+          containerPort: 8080,
+        },
+        minHealthyPercent: 100,
+        healthCheck: {
+          command: [
+            "CMD-SHELL",
+            "curl -f http://localhost:8080/health || exit 1",
+          ],
+          interval: cdk.Duration.seconds(30),
+          retries: 3,
+          startPeriod: cdk.Duration.seconds(15),
+          timeout: cdk.Duration.seconds(5),
+        },
+        // make it public on the internet
+        publicLoadBalancer: true,
+      }
+    );
 
-    new apigateway.LambdaRestApi(this, "HelloApi", {
-      handler: helloFunction,
-      description: "Simple REST API for OP Kiitorata assignment",
-    });
+    // configure ALB to also use the custom healthcheck route
+    fargate.targetGroup.configureHealthCheck({ path: "/health" });
   }
 }
